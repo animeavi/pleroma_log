846ede38023e5939e53e963b5bb3077745dce03c
Zero <zero@cock.li>
2023-05-20 16:33:41 -0400

    No external deps

--- pleroma_log.py
+++ pleroma_log.py
@@ -1,5 +1,7 @@
 import re
-import tailer
+import time
+import select
+import subprocess
 import sys
 
 word_filter = [
@@ -12,19 +14,34 @@ word_filter = [
     "Error while fetching",
     "Couldn't fetch reply@",
     "Could not decode user",
-    "Error while processing object"
+    "Error while processing object",
+    "emoji pack",
+    "[notice] Application",
+    "JOINED chat:public",
+    "Parameters: %{}",
+    "Transport: :websocket",
+    "ONNECTED TO Pleroma.Web.UserSocket",
+    ", \"vsn\" => ",
+    "Serializer: Phoenix.Socket.V2.JSONSerializer",
+    "fetching rich media",
+    "Internal server error",
+    "<no file>"
 ]
 
 time_regex = r"^\d+:\d+:\d+\.\d\d\d "
 log_path = "/var/log/pleroma.log"
 
 
-def print_filtered(line):
-    if not any(word in line for word in word_filter) and not line.isspace():
-        if len(re.findall(time_regex, line)) > 0:
-            print("")
+def print_filtered(line, tail=False):
+    if not any(word in line for word in word_filter):
+        if not tail:
+            if not line.isspace():
+                if len(re.findall(time_regex, line)) > 0:
+                    print("")
 
-        print(line.rstrip())
+                print(line.rstrip())
+        else:
+            print(line)
 
 
 if len(sys.argv) < 2:
@@ -32,8 +49,9 @@ if len(sys.argv) < 2:
     exit(1)
 
 if sys.argv[1] == "tail":
-    for line in tailer.follow(open(log_path)):
-        print_filtered(line)
+    with subprocess.Popen(["tail", "-n0", "-f", log_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as pro:
+        for line in pro.stdout:
+            print_filtered(str(line.decode('utf-8')), True)
 else:
     log_file = open(log_path, 'r')
     lines = log_file.readlines()
